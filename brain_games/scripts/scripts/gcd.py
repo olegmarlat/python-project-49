48115d957f657d05216d9c7c36eba0a3b2d82b1c
Oleg Latypov <latol179@mail.ru>
2023-10-16 22:17:35 +0300

    modifed gcd

--- brain_games/scripts/scripts/gcd.py
+++ brain_games/scripts/scripts/gcd.py
@@ -14,15 +14,15 @@ def game_gcd():
         number2 = randint(1, 50)
         print(f'Question: {number1} {number2}')
         join = prompt.integer('Your answer: ')
-        g_c_d = math.gcd(number1, number2)
-        if join == g_c_d:
+        result = math.gcd(number1, number2)
+        if join == result:
             item += 1
             print('Correct!')
         else:
             item = 4
             print(
                 f"'{join}' is wrong answer ;(. "
-                f"Correct answer was '{g_c_d}'\nLet's "
+                f"Correct answer was '{result}'\nLet's "
                 f"try again, {name}!")
         if item == 3:
             print(f'Congratulations, {name}!')
