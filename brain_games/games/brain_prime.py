c0e1235567f3234c6f1c4b59659905b20a969526
Oleg Latypov <latol179@mail.ru>
2023-11-14 23:00:47 +0300

    changes have been made to the project

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,40 @@
+import prompt
+from random import randint
+import math
+
+def it_prime(num):
+    if num < 2:
+        return 'no'
+    n = 0
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            n += 1
+    if n == 0:
+        return 'yes'
+    else:
+        return 'no'
+def prime_game():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name?")
+    print(f'Hello, {name}!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+
+    item = 0
+    while item < 3:
+        number1 = randint(1, 50)
+        prime = it_prime(number1)
+        print(f'Question: {number1}')
+        join = prompt.string('Your answer: ')
+        if join.lower() == prime:
+            item += 1
+            print('Correct!')
+        else:
+            item = 4
+            print(
+                f"'{join}' is wrong answer ;(. "
+                f"Correct answer was '{prime}'\nLet's "
+                f"try again, {name}!")
+        if item == 3:
+            print(f'Congratulations, {name}!')
+
