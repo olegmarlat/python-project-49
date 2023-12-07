2a648089c3c657456383f46d7ad7500132da5dab
Oleg Latypov <latol179@mail.ru>
2023-12-07 21:37:18 +0300

    mod even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -13,4 +13,4 @@ def game_flow():
             correct_answer = "yes"
         else:
             correct_answer = "no"
-    return str(question), correct_answer
+    return question, correct_answer
