e0303f02f1fbfddc36f230777de54af03b95f2cf
Oleg Latypov <latol179@mail.ru>
2023-12-07 22:18:49 +0300

    modified even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,9 +7,9 @@ GAME_TASK = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 def game_flow():
     item = 0
     while item < 3:
-        question = randint(1, 999999999)
+        question = randint(1, 999999)
         if question % 2 == 0:
             correct_answer = "yes"
         else:
             correct_answer = "no"
-    return question, correct_answer
+    return str(question), correct_answer
