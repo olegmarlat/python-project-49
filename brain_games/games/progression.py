0373a08f176cb8e129be311c97447822eff08319
Oleg Latypov <latol179@mail.ru>
2023-12-08 23:03:22 +0300

    modified progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,14 +5,12 @@ GAME_TASK = 'What number is missing in the progression?'
 
 
 def game_progression():
-    item = 0
-    while item < 3:
-        number1 = randint(1, 10)
-        number2 = randint(100, 150)
-        num_progr = randint(1, 8)
-        numbers = []
-        for i in range(number1, number2, num_progr):
-            numbers.append(i)
+    number1 = randint(1, 10)
+    number2 = randint(100, 150)
+    num_progr = randint(1, 8)
+    numbers = []
+    for i in range(number1, number2, num_progr):
+        numbers.append(i)
         number1 = randint(0, 9)
         correct_answer = numbers[number1]
         numbers[number1] = '..'
