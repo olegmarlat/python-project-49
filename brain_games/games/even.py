bbd0e96fb2dd01672d925dec11e767dacb933944
Oleg Latypov <latol179@mail.ru>
2023-12-08 20:18:04 +0300

    modified:   brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,13 +1,13 @@
 from random import randint
 ''' Generate'''
 
-GAME_TASK = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
+GAME_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def game_flow():
     item = 0
     while item < 3:
         number = randint(1, 999999)
-        question = "{number}"
+        question = {number}
         correct_answer = "yes" if number % 2 == 0 else "no"
     return question, correct_answer
