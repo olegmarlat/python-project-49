dcf602416844dcfd1b7a134535af44d19c90b43b
Oleg Latypov <latol179@mail.ru>
2023-12-08 21:12:29 +0300

    modified calc - step number one

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -15,12 +15,10 @@ def math_operator(addit, subtr, multi):
 
 
 def game_flow():
-    item = 0
     math_list = ["+", "-", "*"]
-    while item < 3:
-        number1 = randint(10, 20)
-        number2 = randint(1, 10)
-        math = random.choice(math_list)
-        question = "{number1} {math} {number2}"
-        correct_answer = math_operator(math, number1, number2)
+    number1 = randint(10, 20)
+    number2 = randint(1, 10)
+    math = random.choice(math_list)
+    question = "{number1} {math} {number2}"
+    correct_answer = math_operator(math, number1, number2)
     return question, correct_answer
