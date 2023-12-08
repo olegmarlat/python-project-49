ef0eb4ed9ac34dba9cd4ceccd403220f8683c48e
Oleg Latypov <latol179@mail.ru>
2023-12-08 22:05:30 +0300

    mod calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,13 +5,13 @@ from random import randint
 GAME_TASK = 'What is the result of the expression?'
 
 
-def math_operator(addit, subtr, multi):
-    if addit == "+":
-        return subtr + multi
-    elif addit == "-":
-        return subtr - multi
-    elif addit == "*":
-        return subtr * multi
+def math_operator(operand, number1, number2):
+    if operand == "+":
+        return number1 + number2
+    elif operand == "-":
+        return number1 - number2
+    elif operand == "*":
+        return number1 * number2
 
 
 def game_flow():
@@ -20,5 +20,5 @@ def game_flow():
     number2 = randint(1, 10)
     math = random.choice(math_list)
     question = f"{number1} {math} {number2}"
-    correct_answer = math_operator(number1, math, number2)
+    correct_answer = math_operator(math, number1, number2)
     return question, correct_answer
