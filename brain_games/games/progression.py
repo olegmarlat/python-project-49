aeec009a2675501d6d9187bf9b9a999cc8cff7a4
Oleg Latypov <latol179@mail.ru>
2023-12-09 00:32:30 +0300

    modified progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,7 +5,7 @@ GAME_TASK = 'What number is missing in the progression?'
 
 
 def game_flow():
-    number1 = randint(1, 9)
+    number1 = randint(2, 9)
     number2 = randint(100, 150)
     num_progr = randint(1, 8)
     numbers = []
@@ -15,4 +15,4 @@ def game_flow():
         correct_answer = numbers[number1]
         numbers[number1] = '..'
         question = " ".join(list(map(str, numbers[0:10])))
-    return question, str(correct_answer)
+        return question, str(correct_answer)
