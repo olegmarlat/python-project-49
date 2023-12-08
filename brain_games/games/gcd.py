94e9ec9d4ea3bbcdfacfe12ed648d246aa1354e4
Oleg Latypov <latol179@mail.ru>
2023-12-08 22:51:32 +0300

    modified gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,6 +8,6 @@ GAME_TASK = 'Find the greatest common divisor of given numbers.'
 def game_flow():
     number1 = randint(1, 50)
     number2 = randint(1, 50)
-    question = ('{number1} {number2}')
+    question = f'{number1} {number2}'
     correct_answer = math.gcd(number1, number2)
     return question, str(correct_answer)
